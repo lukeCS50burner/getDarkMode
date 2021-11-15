50418bb92aa4a494e1f40cb1cc4bfc146eeabd68
lukeCS50burner <85012789+lukeCS50burner@users.noreply.github.com>
2021-11-15 17:16:05 +0000

    Update darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,5 @@
 document.querySelector("html").style.filter = "invert(1) hue-rotate(180deg)";
-
+document.body.style.background = "black";
 
 let media = document.querySelectorAll("video");
  
@@ -19,4 +19,4 @@ media.forEach((mediaItem)=>{
 	mediaItem.style.filter = "invert(1) hue-rotate(180deg)";
 });
 
-document.body.style.background = #000000
+
